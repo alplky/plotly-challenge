19d601ed0a1150721a01553d2ced7df97b64d193
alplky <alicia.pelkey@gmail.com>
2020-11-23 15:25:26 -0600

    created drop down list

--- static/app.js
+++ static/app.js
@@ -1,5 +1,26 @@
-// use D3 to read in samples dataset
+//made sure app.js is linked to html
+alert("Linked up!");
 
+// use D3 to read in samples dataset
+d3.json("../samples.json").then( data => {
+    console.log(data);
+
+    // create the dropdown menu list
+    let select = document.getElementById("selDataset")
+    let dropDowns = data.names;
+    console.log(dropDowns);
+
+    for (let i = 0; i < dropDowns.length; i++) {
+        let opt =  dropDowns[i];
+
+        let el = document.createElement("option");
+        el.text = opt;
+        el.value = opt;
+
+        select.add(el);
+    }
+    
+});
 
 // create horizontal bar chart to display top 10 OTUs for each individual
 
