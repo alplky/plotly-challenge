b72cf40062350e50828d6937c8da86837d40019a
alplky <alicia.pelkey@gmail.com>
2020-11-23 12:50:20 -0600

    rearranging files

---
+++ static/app.js
@@ -0,0 +1,12 @@
+// use D3 to read in samples dataset
+
+
+// create horizontal bar chart to display top 10 OTUs for each individual
+
+
+// create bubble chart that displays each sample
+
+
+// display sample metadata - individual's demographic information of key value pairs
+
+
